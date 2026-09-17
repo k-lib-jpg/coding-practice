dbb11aeeb64e71d0e49be048d44e8c4d9b34adcf
k-lib-jpg <k-liberty080822@outlook.com>
2026-09-17 22:20:47 +0900

    practice 260917

---
+++ 2609/260917.js
@@ -0,0 +1,43 @@
+const longestEvenStreak = (numbers) => {
+ let count = 0;
+ let countNumbers = [];
+ for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    count++;
+  } else {
+    countNumbers.push(count);
+    count = 0;
+  }
+ }
+ countNumbers.push(count);
+ let max = Math.max(...countNumbers);
+ return max;
+}
+
+console.log(
+  longestEvenStreak([1, 2, 4, 6, 3, 8, 10])
+);
+// 3
+
+console.log(
+  longestEvenStreak([2, 4, 6, 8])
+);
+// 4
+
+console.log(
+  longestEvenStreak([1, 3, 5, 7])
+);
+// 0
+
+console.log(
+  longestEvenStreak([2, 4, 3, 6, 8, 10, 5])
+);
+// 3
+
+console.log(
+  longestEvenStreak([2, 3, 4, 6])
+);
+// 1
+
+console.log(longestEvenStreak([]));
+// 0
