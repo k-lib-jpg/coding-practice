8ca8e91fcfdd3ef9c817e6af89312dd81d4fa67d
k-lib-jpg <k-liberty080822@outlook.com>
2026-09-19 23:14:51 +0900

    Practice 260919

---
+++ 2609/260919.js
@@ -0,0 +1,45 @@
+const hasPairWithSum = (numbers, target) => {
+ for (let i = 0; i<numbers.length; i++) {
+  for (let j = i + 1; j<numbers.length; j++) {
+    if (numbers[i] + numbers[j] === target) {
+      return true;
+    } 
+   }
+  }
+ return false;
+ };
+
+console.log(
+  hasPairWithSum([2, 7, 11, 15], 9)
+);
+// true
+
+console.log(
+  hasPairWithSum([1, 2, 3, 4], 10)
+);
+// false
+
+console.log(
+  hasPairWithSum([3, 5, 8, 2], 10)
+);
+// true
+
+console.log(
+  hasPairWithSum([5, 5], 10)
+);
+// true
+
+console.log(
+  hasPairWithSum([5], 10)
+);
+// false
+
+console.log(
+  hasPairWithSum([-3, 1, 4, 7], 4)
+);
+// true
+
+console.log(
+  hasPairWithSum([], 10)
+);
+// false
